2045222ca776887e23c2810ff0c74aa957efabf7
PeterK <kveto002@rangers.uwp.edu>
2022-11-10 17:48:28 -0600

    Iridium convergence graph

--- plot_convergence.py
+++ plot_convergence.py
@@ -11,6 +11,7 @@ def plot_convergence(file_name, convergence_threshold, fit_start_index):
     import matplotlib.pyplot as plt
     from conversions import rydberg_to_eV
     from scipy.optimize import curve_fit
+    from pathlib import Path
 
     data = np.loadtxt(file_name)
     data_labels = file_name.split(".")[3].split("_")
@@ -18,6 +19,26 @@ def plot_convergence(file_name, convergence_threshold, fit_start_index):
     # print(data.transpose())
     x_values = data.transpose()[0]
     y_values = data.transpose()[1]
+
+    atomic_file_name = file_name.split(".")[0] + '.Pm-3m'
+    for name_component in file_name.split(".")[2:]:
+        atomic_file_name += '.' + name_component
+    # print(atomic_file_name)
+
+    path = Path(atomic_file_name)
+    if path.is_file():
+        atomic_data = np.loadtxt(atomic_file_name)
+        atomic_x_values = atomic_data.transpose()[0]
+        atomic_y_values = atomic_data.transpose()[1]
+        Delta_x_values = []
+        Delta_y_values = []
+        for index, x_value in enumerate(x_values):
+            atomic_index = np.where(atomic_x_values == x_value)[0]
+            if len(atomic_index) > 0:
+                Delta_x_values.append(x_value)
+                Delta_y_values.append(y_values[index] - atomic_y_values[atomic_index[0]])
+        x_values = np.array(Delta_x_values)
+        y_values = np.array(Delta_y_values)
 
     # Convert units
     if data_labels[1] == 'totalenergy':
@@ -43,11 +64,10 @@ def plot_convergence(file_name, convergence_threshold, fit_start_index):
     # y_i_converged
     converged_y_value = y_values[converged_index]
 
-
     # Determine axis limits
     #   Set y values up by convergence threshold intervals above and below the minimum value
-    y_minimum = np.min(y_values) - 2*convergence_threshold
-    y_maximum = np.min(y_values) + (8 * convergence_threshold)
+    y_minimum = (y_values[-1]) - 2 * convergence_threshold
+    y_maximum = (y_values[-1]) + (8 * convergence_threshold)
     plt.ylim([y_minimum, y_maximum])
     #   Set x values by lowest y-value appearing off the chart
     off_chart_index = 0
@@ -57,8 +77,8 @@ def plot_convergence(file_name, convergence_threshold, fit_start_index):
     off_chart_index -= 1
     x_minimum = x_values[off_chart_index]
     x_range = x_values[-1] - x_minimum
-    x_maximum = x_minimum + 1.1*x_range
-    plt.xlim([x_minimum, x_maximum])
+    x_maximum = x_minimum + 1.1 * x_range
+    # plt.xlim([x_minimum, x_maximum])
 
     # Plot values
     plt.plot(x_values, y_values, 'o')
@@ -72,7 +92,7 @@ def plot_convergence(file_name, convergence_threshold, fit_start_index):
         # return c0 * c1 * np.power(x - c2, -1) + c3
         #
         # y = d + a e^(-b (x-c)) / (x-c) where b is positive
-        return c0 * np.exp(-c1*(x-c2)) * np.power(x - c2, -1) + c3
+        return c0 * np.exp(-c1 * (x - c2)) * np.power(x - c2, -1) + c3
 
     # Parameter guesses that work
     c3_guess = y_values[-1]
@@ -96,12 +116,13 @@ def plot_convergence(file_name, convergence_threshold, fit_start_index):
     plt.plot(fit_x_values, fit_y_values, color='orange')
 
     # Plot convergence threshold lines
-    plt.axhline(np.min(y_values) - convergence_threshold, linestyle="--", color='black')
-    plt.axhline(np.min(y_values) + convergence_threshold, linestyle="--", color='black')
-
+    # plt.axhline(np.min(y_values) - convergence_threshold, linestyle="--", color='black')
+    # plt.axhline(np.min(y_values) + convergence_threshold, linestyle="--", color='black')
+    plt.axhline((y_values[-1]) - convergence_threshold, linestyle="--", color='black')
+    plt.axhline((y_values[-1]) + convergence_threshold, linestyle="--", color='black')
     # Plot convergence point vertical line segment
     plt.axvline(x_values[converged_index],
-                ymax=(y_values[converged_index]-y_minimum)/(y_maximum-y_minimum), linestyle="-.", color="black")
+                ymax=(y_values[converged_index] - y_minimum) / (y_maximum - y_minimum), linestyle="-.", color="black")
 
     # Label convergence thresholds
     upper_threshold_label = f'+{convergence_threshold:.3f} eV'
@@ -113,27 +134,31 @@ def plot_convergence(file_name, convergence_threshold, fit_start_index):
 
     plot_y_minimum, plot_y_maximum = plt.ylim()
     plot_y_range = plot_y_maximum - plot_y_minimum
-    upper_threshold_label_y = 0.05 * plot_y_range + (np.min(y_values) + convergence_threshold)
-    lower_threshold_label_y = -0.05 * plot_y_range + (np.min(y_values) - convergence_threshold)
+    upper_threshold_label_y = 0.05 * plot_y_range + ((y_values[-1]) + convergence_threshold)
+    lower_threshold_label_y = -0.05 * plot_y_range + ((y_values[-1]) - convergence_threshold)
     plt.text(threshold_label_x, upper_threshold_label_y, upper_threshold_label, va='center')
     plt.text(threshold_label_x, lower_threshold_label_y, lower_threshold_label, va='center')
-
 
     # Label axes
     if data_labels[0] == 'ecutwfc':
         x_label = r'$E_\mathrm{cut}$ (Ry)'
     if data_labels[1] == 'totalenergy':
         y_label = r'$E_\mathrm{total}$ (eV)'
+        if path.is_file():
+            y_label = r'$\Delta E_\mathrm{total}$ (eV)'
+
     plt.xlabel(x_label)
     plt.ylabel(y_label)
 
     # Label plot
-    chemical_label = file_name.split('.')[0].split('/')[1]
+    # chemical_label = file_name.split('.')[0].split('/')[1]
+    chemical_label = file_name.split('.')[0]
+
     structure_label = file_name.split('.')[1]
     #    Convert "-" to overline
     if len(structure_label.split('-')) > 1:
-        structure_label = r'$' + structure_label.split('-')[0] + '\overline{' + structure_label.split('-')[1][0] + '}' +\
-                      structure_label.split('-')[1][1:] + '$'
+        structure_label = r'$' + structure_label.split('-')[0] + '\overline{' + structure_label.split('-')[1][0] + '}' + \
+                          structure_label.split('-')[1][1:] + '$'
     exchange_correlation_label = file_name.split('.')[2]
     plot_label = chemical_label + ' ' + structure_label + ' ' + exchange_correlation_label
     plot_label_x = plot_x_maximum - 0.05 * plot_x_range
@@ -144,16 +169,13 @@ def plot_convergence(file_name, convergence_threshold, fit_start_index):
     """
     Need more cutoff energies for Ir Fm-3m  :(
     """
+    print(x_values, y_values)
     return
 
 
 if __name__ == '__main__':
     # file_of_interest = 'data/Ir.Fm-3m.PBEsol.ecutwfc_totalenergy.txt'
-    file_of_interest = 'data/Pt.Fm-3m.PBEsol.ecutwfc_totalenergy.txt'
+    file_of_interest = 'Ir.Fm-3m.PBEsol.ecutwfc_totalenergy.txt'
     energy_convergence_threshold = 0.001  # eV
-    start_index_for_fit = 15
+    start_index_for_fit = 0
     plot_convergence(file_of_interest, energy_convergence_threshold, start_index_for_fit)
-
-
-
-
