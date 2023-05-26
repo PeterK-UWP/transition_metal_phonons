f90a03a73a2db8151bdc8eb0419ff510e58dcb4d
williamdparker <parkerw@uwp.edu>
2023-05-26 13:34:22 -0500

    Progress toward producing LaTeX table of EoS quantities

--- generate_dictionary.py
+++ generate_dictionary.py
@@ -1,11 +1,26 @@
-symmetries = ['Im', 'Pm3m', 'P4mmm', 'Cmmm']
+def generate_dictionary(symmetries, values,
+                        quantities=['equilibrium_volume', 'cohesive_energy', 'bulk_modulus', 'bulk_modulus_derivative'],
+                        units=[r'$\AA^3$/f.u.', 'eV/f.u.', 'GPa', ' '],
+                        symbols=[r'$V_0$', r'$E_\textrm{coh}$', r'$K_0$', r'$K^\prime_0$']):
 
-quantities = ['equilibrium_volume', 'cohesive_energy', 'bulk_modulus', 'bulk_modulus_derivaitve']
+    nested_dictionary_template = {}
+    for quantity, unit, symbol in zip(quantities, units, symbols):
+        nested_dictionary_template[quantity] = {'units': unit, 'symbol': symbol}
 
-units = ['$\AA^3$/f.u.', 'eV/f.u.', 'GPa', ' ']
+    dictionary = {}
+    value_index = 0
+    for symmetry in symmetries:
+        dictionary[symmetry] = nested_dictionary_template
+        for quantity in quantities:
+            dictionary[symmetry][quantity]['value'] = values[value_index]
+            value_index += 1
+    return dictionary
 
-symbols = ['V_0', 'E_\textrm{coh}', 'K_0', 'K^\prime_0']
 
-dictionary = {}
-for symmetry in symmetries:
-    dictionary[symmetry] =
+if __name__ == '__main__':
+    import numpy as np
+    test_symmetries = ['Im', 'Pm3m', 'P4mmm', 'Cmmm']
+    test_values = np.zeros(len(test_symmetries)*4)
+
+    test_dictionary = generate_dictionary(test_symmetries, test_values)
+    print(test_dictionary)
---
+++ generate_latex_table.py
@@ -0,0 +1,63 @@
+def generate_latex_table(quantities):
+    number_of_keys = len(quantities)
+
+    latex_table = "\\begin{tabular}{" + number_of_keys*'l' + "}\n"
+    latex_table += f'{"Quantity":17}'
+
+    for symmetry in quantities.keys():
+        symmetry = rf"$" + f'{symmetry}' + rf"$"
+        latex_table += rf"& {symmetry:>10} "
+    latex_table += "\\\\\n"
+    latex_table += "\\hline\n"
+    first_symmetry = next(iter(quantities))
+    for quantity in quantities[first_symmetry].keys():
+        latex_table += f'{quantities[first_symmetry][quantity]["symbol"]:17}'
+        for current_symmetry in quantities.keys():
+            latex_table += f'& {quantities[current_symmetry][quantity]["value"]:10} '
+        latex_table += "\\\\\n"
+
+    latex_table += "\\end{tabular}"
+    return latex_table
+
+
+if __name__ == "__main__":
+    from equation_of_state.two_column_text_read import two_column_text_read
+    from equation_of_state.convert_units import convert_units
+    from equation_of_state.quadratic_fit import quadratic_fit
+    from equation_of_state.equations_of_state import fit_eos
+    from generate_dictionary import generate_dictionary
+    import numpy as np
+
+    # test_symmetries = ['Im', 'Pm-3m', 'P4mmm']
+    # test_values = np.zeros(len(test_symmetries)*4)
+    # test_dictionary = generate_dictionary(test_symmetries, test_values)
+    # output_table = generate_latex_table(test_dictionary)
+    # print()
+    # print(output_table)
+
+    symmetries = []
+    values = []
+
+    for symmetry in ['Im', 'Pm-3m', 'P4mmm']:
+        filename = "TiIr." + symmetry + ".PBEsol.volume_totalenergy.txt"
+        directory = "data"
+
+        volumes_energies = two_column_text_read(f'{directory}/{filename}')
+
+        volumes_energies = np.array([convert_units(volumes_energies[0], 'cb/a'),
+                                     convert_units(volumes_energies[1], 'rb/a')])
+
+        quadratic_coefficients = quadratic_fit(volumes_energies)
+
+        equation_of_state, equation_of_state_parameters = fit_eos(volumes_energies[0], volumes_energies[1],
+                                                                  quadratic_coefficients)
+
+        symmetries.append(symmetry)
+
+        # Need to calculate E_coh from E_0 and order to V_0, E_coh, K_0, K_0'
+        for parameter in equation_of_state_parameters:
+            values.append(parameter)
+
+    eos_dictionary = generate_dictionary(symmetries, values)
+    eos_latex_table = generate_latex_table(eos_dictionary)
+    print(eos_latex_table)
